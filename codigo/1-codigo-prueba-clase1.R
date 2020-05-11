f12dd023e46ddff535e694966d17bd62e1ce10f3
Felipe Ruiz Bruzzone <felipe.ruiz@ug.uchile.cl>
2020-05-11 12:40:41 -0400

    Subida archivos para prueba clase 1

--- codigo/1-codigo-prueba-clase1.R
+++ codigo/1-codigo-prueba-clase1.R
@@ -3,10 +3,17 @@
 # ESTACIÓN LASTARRIA - MAYO/JUNIO 2020
 # PROFESOR: FELIPE RUIZ
 
-
 # CARGAR BASE DE DATOS
 datos <- read.csv2("datos/paraguay.csv")
 
-# GUARDAR BASE DE DATOS EN FORMATO R
+# GUARDAR BASE DE DATOS EN FORMATO R (EN CARPETA DATOS)
 
 saveRDS(datos, file = "datos/paraguay_rds.rds")
+
+
+# GUARDAR RESULTADO DE UN ANÁLISIS (EN CARPETA RESULTADOS)
+resultado1 <- summary(datos$edad)
+
+install.packages("openxlsx")
+library(openxlsx)
+write.xlsx(resultado1, file = "resultados/impresora.xlsx")
